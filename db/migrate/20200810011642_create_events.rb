3798fa562c60bfd9f825a8bec351f2a49e35782b
carrollm2 <carrollm2@gmail.com>
2020-08-11 17:52:45 -0400

    added destination_id field to create_events migration file

--- db/migrate/20200810011642_create_events.rb
+++ db/migrate/20200810011642_create_events.rb
@@ -1,6 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.integer :destination_id
       t.text :description
       t.string :ticket_site
       t.date :event_date
