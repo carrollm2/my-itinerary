1e889ef697ac24fa1fb78a28e599a5c4cfbce14a
carrollm2 <carrollm2@gmail.com>
2020-08-13 22:00:34 -0400

    added create action  and strong params

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,3 +1,20 @@
 class ItinerariesController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def new
+    end
+
+    def create
+        @itinerary = current_user.itineraries.build(itinerary_params)
+        if @itinerary.save
+            redirect_to @itinerary
+        else
+            render :new
+        end
+    end
+
+    private
+    def itinerary_params
+        params.require(:post).permit(:notes)
+    end
 end
