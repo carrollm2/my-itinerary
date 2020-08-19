7fde3375cb948c453a1dfc5eb882f8438fc04f8a
carrollm2 <carrollm2@gmail.com>
2020-08-18 23:58:57 -0400

    added nested route for index action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,18 @@
 class EventsController < ApplicationController
+
+    def index 
+        if params[:user_id]
+            @user = User.find_by_id(params[:user_id])
+
+            if @user
+                @events = @user.events
+            else
+                @error = "That user does not exist"
+                @events = Event.all
+            end
+        else
+            @events = Event.all
+        end
+    end
+
 end
