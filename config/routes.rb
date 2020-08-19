4953f5eeff3cfcd6c6e3d5a95a759594a5f960ca
carrollm2 <carrollm2@gmail.com>
2020-08-18 23:59:58 -0400

    created nest routes for user events

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
   resources :events
   resources :destinations
   resources :itineraries
-  resources :users
+  # resources :itineraries do
+  #   resources :events, only: [:new, :create, :index]
+  # end
+
+  resources :users do
+    resources :events, only: [:new, :create, :index]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
