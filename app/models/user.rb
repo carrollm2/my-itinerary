189bb719d303762ef8c20e4d5c678b1416e6f2f9
carrollm2 <carrollm2@gmail.com>
2020-08-18 23:41:44 -0400

    added has many events and destinations through user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_many :itineraries
+    has_many :events, through: :itineraries
+    has_many :destinations, through: :itineraries
+
 
     validates :username, presence: true
     validates :email, presence: true
