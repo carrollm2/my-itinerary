0a1b36b59464fc6361d8f1a6e63bf62456a9d857
carrollm2 <carrollm2@gmail.com>
2020-08-20 22:51:46 -0400

    added migrations for admin column in user table

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_011642) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
