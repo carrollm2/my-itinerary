e173b6e1cd215b3ef5fed57bc33733898f23b6cd
carrollm2 <carrollm2@gmail.com>
2020-08-20 21:17:51 -0400

    added nested routes to allow user able to create events for a destination

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,31 @@
 class EventsController < ApplicationController
 
+
+    def new
+        @destination = Destination.find_by_id(params[:destination_id])
+
+        if @destination
+            @event = @destination.events.build
+        else
+            flash[:message] = "destination does not exist"
+            redirect_to destinations_path            
+        end
+    end 
+
+
+    def create
+        destination = Destination.find_by_id(params[:destination_id])
+
+        @event = destination.events.build(event_params).save
+ 
+        if @event
+            redirect_to destination_path(destination)
+        else
+            render :new
+        end
+    end
+
+
     def index 
         if params[:user_id]
             @user = User.find_by_id(params[:user_id])
@@ -8,11 +34,16 @@ class EventsController < ApplicationController
                 @events = @user.events
             else
                 @error = "That user does not exist"
-                @events = Event.all
+                @events = Event.alls
             end
         else
             @events = Event.all
         end
     end
+
+    private
+    def event_params
+        params.require(:event).permit(:description, :destination_id, :ticket_site, :event_date)
+    end    
 
 end
