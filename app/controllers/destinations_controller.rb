5c5867474ccf203231ac780fc32c4fe9ce8cf891
carrollm2 <carrollm2@gmail.com>
2020-08-20 21:13:07 -0400

    added new, create, index and show routes along with strong params

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,35 @@
 class DestinationsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @destination = Destination.new(destination_params)
+        if @destination.save
+            redirect_to @destination
+        else
+            render :new
+        end        
+    end
+
+
+    def index
+        @destinations = Destination.all
+    end
+
+    def show
+        @destination = Destination.find_by_id(params[:id])
+    end
+
+
+
+    def index
+        @destinations = Destination.all
+    end
+
+    private
+    def destination_params
+        params.require(:destination).permit(:city)
+    end    
+
 end
