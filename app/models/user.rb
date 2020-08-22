c69d3c0eca6a3cf4d2bf6344eb4cb4df5932f3b8
carrollm2 <carrollm2@gmail.com>
2020-08-21 22:58:52 -0400

    added password_confirmation to user_params

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,24 @@ class User < ApplicationRecord
     has_many :events, through: :itineraries
     has_many :destinations, through: :itineraries
 
-
-    validates :username, presence: true
-    validates :email, presence: true
     has_secure_password
+
+
+    validates :username, :email, presence: { message: "must be populated" }, on: :create
+    validates :username, :email, uniqueness: { message: "previously used by another user. Please choose a different %{attribute}" }, on: :create
+
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, on: :create
+
+    validates :password, :length => { :in => 6..40 }, on: :create
+
+    validate :password_complexity, on: :create
+
+    validates_confirmation_of :password
+
+    def password_complexity
+        return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    
+        errors.add :password, 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    end    
 
 end
