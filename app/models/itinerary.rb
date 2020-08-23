503ac7610c3061c120754970719d791dee69efb8
carrollm2 <carrollm2@gmail.com>
2020-08-22 21:55:35 -0400

    added itinerary validations for create method

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,4 +2,7 @@ class Itinerary < ApplicationRecord
     belongs_to :user
     belongs_to :destination
     belongs_to :event
+
+    validates :name, :destination_id, :event_id, presence: { message: "must be populated" }, on: :create
+
 end
