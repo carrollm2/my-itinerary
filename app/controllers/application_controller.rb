8d06a5618ae22c848e89d56dadab52960e2f77ca
carrollm2 <carrollm2@gmail.com>
2020-08-23 23:49:21 -0400

    added authorized_user_itineraries helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user, :logged_in?, :is_admin?
+    helper_method :current_user, :logged_in?, :is_admin?, :get_user_itinerary
 
     private
     def current_user
@@ -19,4 +19,23 @@ class ApplicationController < ActionController::Base
         current_user.admin
     end
 
+    def authorized_user_itineraries(action)
+        if params[:user_id]
+            @user = User.find_by_id(params[:user_id])
+
+            if @user == current_user
+                if action == "index"
+                    @itineraries = @user.itineraries
+                else
+                    @itinerary = Itinerary.find(params[:id])
+                end
+            else
+                flash[:message] = "Not authorized to see other user's itineraries"
+                redirect_to root_path
+            end
+        else
+            flash[:message] = "Not authorized to see other user's itineraries"
+            redirect_to root_path 
+        end   
+    end
 end
