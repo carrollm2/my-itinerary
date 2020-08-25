5458181816de794bdc4f47fe0a75679a8f4b3184
carrollm2 <carrollm2@gmail.com>
2020-08-24 21:15:42 -0400

    removed get_user_itinerary helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,23 +19,4 @@ class ApplicationController < ActionController::Base
         current_user.admin
     end
 
-    def authorized_user_itineraries(action)
-        if params[:user_id]
-            @user = User.find_by_id(params[:user_id])
-
-            if @user == current_user
-                if action == "index"
-                    @itineraries = @user.itineraries
-                else
-                    @itinerary = Itinerary.find(params[:id])
-                end
-            else
-                flash[:message] = "Not authorized to see other user's itineraries"
-                redirect_to root_path
-            end
-        else
-            flash[:message] = "Not authorized to see other user's itineraries"
-            redirect_to root_path 
-        end   
-    end
 end
