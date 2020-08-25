137b5e358d73dd8847e27b0b54786989517bd24a
carrollm2 <carrollm2@gmail.com>
2020-08-24 21:44:44 -0400

    added custom validation to check if selected event occurs in selected destination

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -6,4 +6,13 @@ class Itinerary < ApplicationRecord
     validates :name, :destination_id, :event_id, presence: { message: "must be populated" }, on: :create
     validates_uniqueness_of :user_id, :scope => [:destination_id, :event_id], :message=>"has previously created identical itinerary", on: :create
 
+    validate :event_in_destination?, on: :create
+
+    def event_in_destination?
+        if !destination.events.include?(event)
+            errors.add :event, 'selected does not take place in Destination selected'
+        end
+    end    
+
+
 end
