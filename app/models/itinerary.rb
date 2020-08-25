b71036c30a70c4d708c71dadfb363bd5f9d65ab6
carrollm2 <carrollm2@gmail.com>
2020-08-24 21:14:12 -0400

    add validation test preventing user creating identical itineraries

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -4,5 +4,6 @@ class Itinerary < ApplicationRecord
     belongs_to :event
 
     validates :name, :destination_id, :event_id, presence: { message: "must be populated" }, on: :create
+    validates_uniqueness_of :user_id, :scope => [:destination_id, :event_id], :message=>"has previously created identical itinerary", on: :create
 
 end
