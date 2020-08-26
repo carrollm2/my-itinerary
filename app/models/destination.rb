61412e6e3ebd743083b858ae3b9916e55ffb9916
carrollm2 <carrollm2@gmail.com>
2020-08-25 23:44:44 -0400

    added validations for city uniqueness and format

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,6 +1,12 @@
 class Destination < ApplicationRecord
     has_many :itineraries
     has_many :events
-    
     has_many :users, through: :itineraries
+
+    validates_uniqueness_of :city, :case_sensitive => false, :message => "already exists in database", on: :create
+
+    @city_regex = /[^,]*,[^,]*/
+    validates :city, 
+        :format => { :with => @city_regex, :message => "Invalid city format. Must be 'city, state' format" }
+
 end
