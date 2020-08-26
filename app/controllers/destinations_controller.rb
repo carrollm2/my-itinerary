721e8b18dd146d0eff3bb0e941dfcc3aed7e63e6
carrollm2 <carrollm2@gmail.com>
2020-08-26 00:19:05 -0400

    added edit and delete actions

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -23,6 +23,24 @@ class DestinationsController < ApplicationController
     end
 
 
+    def edit 
+        @destination = Destination.find(params[:id])
+    end
+
+    def update
+        @destination = Destination.find(params[:id])
+        @destination.update(destination_params)
+        redirect_to destination_path(@destination)
+    end
+
+
+    def destroy
+        @destination = Destination.find(params[:id])
+        @destination.destroy
+        redirect_to destinations_path(@destination)
+    end
+
+
     private
     def destination_params
         params.require(:destination).permit(:city)
