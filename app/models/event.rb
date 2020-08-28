37758e0e6c46c9de579733acdfd222489b57333f
carrollm2 <carrollm2@gmail.com>
2020-08-27 23:19:58 -0400

    added validations for presence and uniqueness

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ApplicationRecord
     belongs_to :destination
     has_many :itineraries
     has_many :users, through: :itineraries
+
+    validates :description, :destination_id, :event_date, presence: { message: "must be populated" }, on: :create
+    validates_length_of :ticket_site, minimum: 5, maximum: 200, allow_blank: true
+
+    validates_uniqueness_of :description, :scope => [:destination_id, :event_date], :message=>"matches event occurring in same destination and on the same date", on: :create
+
 end
