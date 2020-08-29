bbf1ac6528f86cc319c31dcd53f948883193b336
carrollm2 <carrollm2@gmail.com>
2020-08-29 10:59:45 -0400

    added non_auth_login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,21 +31,35 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.username = auth['info']['name']
-      u.email = auth['info']['email']
-    end
 
-    @user.save(:validate => false)
- 
-    session[:user_id] = @user.id
- 
-    redirect_to user_path(@user)
+    if auth
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.username = auth['info']['name']
+        u.email = auth['info']['email']
+      end
+
+      @user.save(:validate => false)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      non_auth_login
+    end
   end
  
   private
  
   def auth
     request.env['omniauth.auth']
+  end
+
+  def non_auth_login
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "incorrect login info, please try again"
+      redirect_to '/login'
+    end    
   end
 end
