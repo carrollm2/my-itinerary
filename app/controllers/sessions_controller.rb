37b2c6074a134786ed3f541da240ac4bdf5f2e15
carrollm2 <carrollm2@gmail.com>
2020-08-29 01:13:03 -0400

    updated create action to allow users to sign up and login with google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,28 @@
+# class SessionsController < ApplicationController
+#   def home
+#   end
+
+#   def destroy
+#     session.clear
+#     redirect_to root_path
+#   end
+
+#   def create
+#     user = User.find_by(username: params[:user][:username])
+#     if user && user.authenticate(params[:user][:password])
+#       session[:user_id] = user.id
+#       redirect_to user_path(user)
+#     else
+#       flash[:message] = "incorrect login info, please try again"
+#       redirect_to '/login'
+#     end
+#   end
+# end
+
+
 class SessionsController < ApplicationController
-  def home
+
+  def home   
   end
 
   def destroy
@@ -8,13 +31,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:user][:username])
-    if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user)
-    else
-      flash[:message] = "incorrect login info, please try again"
-      redirect_to '/login'
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['name']
+      u.email = auth['info']['email']
     end
+
+    @user.save(:validate => false)
+ 
+    session[:user_id] = @user.id
+ 
+    redirect_to user_path(@user)
+  end
+ 
+  private
+ 
+  def auth
+    request.env['omniauth.auth']
   end
 end
