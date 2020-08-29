83d9d6a893bd96fa46fccc5442c2493a3cb045e5
carrollm2 <carrollm2@gmail.com>
2020-08-29 01:15:39 -0400

    added user info collected from google if user signs in with google

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     end
 
     def create
+
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
@@ -22,6 +23,6 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :email, :password, :password_confirmation, :admin)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :admin, :uid)
     end
 end
