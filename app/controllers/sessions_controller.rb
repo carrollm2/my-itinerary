d8e040ae10582e512c27301f5a6e20f3413e9c36
carrollm2 <carrollm2@gmail.com>
2020-09-01 21:36:39 -0400

    removed old commented code for session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,3 @@
-# class SessionsController < ApplicationController
-#   def home
-#   end
-
-#   def destroy
-#     session.clear
-#     redirect_to root_path
-#   end
-
-#   def create
-#     user = User.find_by(username: params[:user][:username])
-#     if user && user.authenticate(params[:user][:password])
-#       session[:user_id] = user.id
-#       redirect_to user_path(user)
-#     else
-#       flash[:message] = "incorrect login info, please try again"
-#       redirect_to '/login'
-#     end
-#   end
-# end
-
-
 class SessionsController < ApplicationController
 
   def home   
