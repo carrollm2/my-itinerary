7d480b0e3a8ddcc315ab16203ed837e7a4d0558e
carrollm2 <carrollm2@gmail.com>
2020-09-01 22:09:37 -0400

    added flash message for successful sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
+            flash[:success] = "You have successfully created an account! Welcome to MyItinerary!"
             redirect_to user_path(@user)
         else
             render :new
