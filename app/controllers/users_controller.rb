434cd09cc5fb66a0c2c8de35dd719d8b08d8a305
carrollm2 <carrollm2@gmail.com>
2020-09-01 21:04:03 -0400

    added redirect_if_not_logged_in to controller before action show only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 class UsersController < ApplicationController
+    before_action :redirect_if_not_logged_in, only: [:show]
+
     def new
         @user = User.new
     end
 
     def create
-
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
-            # redirect_to new_user_path, :flash => { :error => @user.errors.full_messages }
         end
     end
 
     def show
-        redirect_if_not_logged_in
         @user = User.find_by_id(params[:id])
         redirect_to root_path if !@user
     end
