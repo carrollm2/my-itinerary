3d5f1b0d89db9a48d53738bc59d47b310352e60d
carrollm2 <carrollm2@gmail.com>
2020-09-14 20:26:35 -0400

    updated event date info for each destination

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,20 @@ destination_1.events.build(description: "Rob Schneider Comedy Show", ticket_site
 
 
 destination_2 = Destination.create(city: "Park City, UT")
-destination_2.events.build(description: "Tour de Suds Bike Tour", ticket_site: "https://tourdessuds.athlete360.com", event_date: DateTime.new(2020, 9, 13, 12, 00)).save
-destination_2.events.build(description: "Sunday Funday: Open Studio Park City", ticket_site: "https://app.getoccasion.com/xp/v3xs2SNT?time_slot_id=gubrq6Wu7oQP", event_date: DateTime.new(2020, 9, 16, 12, 00)).save
+destination_2.events.build(description: "Tour de Suds Bike Tour", ticket_site: "https://tourdessuds.athlete360.com", event_date: DateTime.new(2020, 10, 13, 12, 00)).save
+destination_2.events.build(description: "Sunday Funday: Open Studio Park City", ticket_site: "https://app.getoccasion.com/xp/v3xs2SNT?time_slot_id=gubrq6Wu7oQP", event_date: DateTime.new(2020, 10, 16, 12, 00)).save
 
 
 destination_3 = Destination.create(city: "White Fish, MT")
 destination_3.events.build(description: "Brews and Views Craft Brewery Tour", event_date: DateTime.new(2020, 9, 4, 15, 00)).save
-destination_3.events.build(description: "Brash Rodeo Summer Series", ticket_site: "https://glaciercountryrodeo.com/event/sept-5-rodeo/", event_date: DateTime.new(2020, 9, 5, 18, 00)).save
+destination_3.events.build(description: "Brash Rodeo Summer Series", ticket_site: "https://glaciercountryrodeo.com/event/sept-5-rodeo/", event_date: DateTime.new(2020, 12, 5, 18, 00)).save
 
 
 destination_4 = Destination.create(city: "Madison, WI")
 destination_4.events.build(description: "Bitters Boot Camp", event_date: DateTime.new(2020, 12, 12, 14, 00)).save
-destination_4.events.build(description: "Madison Ghost Walks - King Street Spirits", ticket_site: "https://americanghostwalks.com/haunted-madison-ghost-tours/", event_date: DateTime.new(2020, 9, 5, 17, 30)).save
+destination_4.events.build(description: "Madison Ghost Walks - King Street Spirits", ticket_site: "https://americanghostwalks.com/haunted-madison-ghost-tours/", event_date: DateTime.new(2021, 1, 5, 17, 30)).save
 
 
 destination_5 = Destination.create(city: "Bend, Oregon")
-destination_5.events.build(description: "Dave Matthews Band", ticket_site: "https://bendconcerts.com/event/dave-matthews-band/", event_date: DateTime.new(2020, 9, 9, 17, 00)).save
-destination_5.events.build(description: "Flight Night at Crater Lake Spirits", ticket_site: "https://www.facebook.com/craterlakespiritstastingroom", event_date: DateTime.new(2020, 9, 21, 17, 30)).save
+destination_5.events.build(description: "Dave Matthews Band", ticket_site: "https://bendconcerts.com/event/dave-matthews-band/", event_date: DateTime.new(2020, 10, 9, 17, 00)).save
+destination_5.events.build(description: "Flight Night at Crater Lake Spirits", ticket_site: "https://www.facebook.com/craterlakespiritstastingroom", event_date: DateTime.new(2020, 10, 21, 17, 30)).save
